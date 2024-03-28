a24333330dd6743f5650650af7566c7e49018dbc
Arizli Romadhon <pprofecta@gmail.com>
2024-03-28 14:49:33 +0700

    fixing form-apply tag

--- src/pages/form-apply.jsx
+++ src/pages/form-apply.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Wrapper from "../layout/wrapper";
 import SEO from "../common/seo";
-
+import FormApply from "../components/form-apply/form-apply";
 
 const index = () => {
   return (
     <Wrapper>
-      <SEO pageTitle={ "PROFECTA PERDANA | Form-Apply" } />
+      <SEO pageTitle={"PROFECTA PERDANA | Form-Apply"} />
       <FormApply />
     </Wrapper>
   );
