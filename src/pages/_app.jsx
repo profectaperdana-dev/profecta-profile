2580274df1e26ed524e16b003ee3f26deced1959
Arizli Romadhon <pprofecta@gmail.com>
2024-04-26 14:37:11 +0700

    adding vercel web analytic and speed insight

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,17 @@
-import "@/src/styles/index.scss";  
-         
+import "@/src/styles/index.scss";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 if (typeof window !== "undefined") {
   require("bootstrap/dist/js/bootstrap");
 }
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
