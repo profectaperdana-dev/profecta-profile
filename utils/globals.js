bfa0c6cb8b1fa03476f6daaf4f2cb8b3d86ee2a4
Arizli Romadhon <pprofecta@gmail.com>
2024-04-29 15:12:27 +0700

    changing base url

--- utils/globals.js
+++ utils/globals.js
@@ -1,5 +1,5 @@
 const url = {
-  PROFECTA_API_URL: "https://testing.profectaperdana.com",
+  PROFECTA_API_URL: "https://apps.profectaperdana.com",
   PROFECTA_PROFILE_URL: "http://localhost:3001",
 };
 
