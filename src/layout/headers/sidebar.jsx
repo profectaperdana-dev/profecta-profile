2e306a0dd378b65afe13eabe79a5916e2b03a61b
profectaperdana-dev <110365734+profectaperdana-dev@users.noreply.github.com>
2025-03-09 19:43:22 +0700

    Update sidebar.jsx

--- src/layout/headers/sidebar.jsx
+++ src/layout/headers/sidebar.jsx
@@ -13,12 +13,33 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
                 </div>
                 <div className="mobile-menu mean-container">
                     <MobileMenus />
+                    
+                    <div className="sidebar-title">
+                        <h3>Contact Us</h3>
+                    </div>
+                    <br></br>
                     <div className="sidebar-title">
                         <h5>Palembang</h5>
                     </div>
                     <ul className="sidebar-list">
                         <li>Jl. Brigjen. H. Kasim, Ruko Jaya Raya, No.12 Palembang - South Sumatera</li>
                         <li>0711-8254-56</li>
+                    </ul>
+
+                    <div className="sidebar-title">
+                        <h5>Jambi</h5>
+                    </div>
+                    <ul className="sidebar-list">
+                        <li>Jl.Abdul Rahman Saleh Kel.Pasir Putih Kec.Jambi Selatan, Kota Jambi</li>
+                        <li>0811-7819-980</li>
+                    </ul>
+
+                    <div className="sidebar-title">
+                        <h5>Bengkulu</h5>
+                    </div>
+                    <ul className="sidebar-list">
+                        <li>Jl. Kapuas no.17 RT 014 / Rw 004 , Padang Harapan , Gading cempaka, Kota Bengkulu</li>
+                        <li>0811-6119-009</li>
                     </ul>
                     
                     <div className="tp-sidebar-social">
